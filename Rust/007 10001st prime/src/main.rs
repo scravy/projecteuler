df6f28aa63b2f03ae442356fc14ba7368bbfa1ab
Julian Fleischer <tirednesscankill@warhog.net>
2020-06-23 12:33:43 +0200

    Rust: 007 10001st prime

---
+++ Rust/007 10001st prime/src/main.rs
@@ -0,0 +1,22 @@
+use std::collections::BTreeSet;
+
+type Num = u64;
+
+fn main() {
+    let mut primes: BTreeSet<Num>= BTreeSet::new();
+    let mut n: Num = 1;
+    'outer: while primes.len() < 10001 {
+        n += 1;
+        let s = (n as f64).sqrt() as Num;
+        for &p in primes.iter() {
+            if p > s {
+                break;
+            }
+            if n % p == 0 {
+                continue 'outer;
+            }
+        }
+        primes.insert(n);
+    }
+    println!("{}", n);
+}
